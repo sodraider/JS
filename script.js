bce5985397be3d875efadf1541f1f73b4ae729f9
Dmitry <sodraider@gmail.com>
2020-05-04 21:34:26 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// alert(1);
+// [0,3].push;
+//comment
+// комментарии
+// 2 строчки
+var leftBorderWidth = 1;
+
+let second = 2;
+const pi = 3.14;
+console.log(pi);
+var number = 5;
+var string = "Hello, World";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+var Persone = {
+    name: "Dima",
+    age: "29",
+    isMerried: false
+}
+console.log(Persone.name);
+console.log(Persone["age"]);
+
+let arr = [1,2,3,4];
+console.log(arr[0]);
+// let answer = confirm("Are you happy?");
+// console.log(answer);
+let answer = prompt("Are you happy?", "");
+console.log( typeof(answer) );
