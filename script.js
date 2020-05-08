fb7f642bb2b5ff6f7dc807e9959fe13fcdb801e4
Dmitry <sodraider@gmail.com>
2020-05-08 19:22:26 +0300

    Урок 3. Обернул еще несколько функций

--- script.js
+++ script.js
@@ -68,19 +68,21 @@ chooseExpenses();
     
 // } while (i < 2);
 
+function detectDayBudget(){
+    appData.moneyPerDay = (appData.budget/30).toFixed();
+    alert("Ваш ежедневный бюджет:" + appData.moneyPerDay);
+}
 
-appData.moneyPerDay = (appData.budget/30).toFixed();
-
-alert("Ваш ежедневный бюджет:" + appData.moneyPerDay);
-
-if (appData.moneyPerDay < 100){
-    console.log("Минимальный уровень достатка");
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
-    console.log("Средний уровень достатка");
-} else if (appData.moneyPerDay > 2000){
-    console.log("Высокий уровень достатка");
-} else {
-    console.log("Ошибка");
+function detectLevel(){
+    if (appData.moneyPerDay < 100){
+        console.log("Минимальный уровень достатка");
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
+        console.log("Средний уровень достатка");
+    } else if (appData.moneyPerDay > 2000){
+        console.log("Высокий уровень достатка");
+    } else {
+        console.log("Ошибка");
+    }
 }
 
 function checkSavings (){
@@ -92,3 +94,9 @@ function checkSavings (){
     }
 }
 checkSavings();
+
+function chooseOptExpenses (){
+    for (let i = 1; i <= 3; i++){
+    appData.optionalExpenses[i] = prompt("Статья необязательных расходов",'');
+    }
+}
